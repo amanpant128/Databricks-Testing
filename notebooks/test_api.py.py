f60cae5180621b8bf6754c63247ad693fe396903
amanpant128 <amanpant128>
2025-08-19 07:35:03 +0000

    sonarpush

--- notebooks/test_api.py.py
+++ notebooks/test_api.py.py
@@ -15,14 +15,14 @@ class TestApi(unittest.TestCase):
         mock_df = MagicMock()
         mock_spark.builder.appName.return_value.getOrCreate.return_value.createDataFrame.return_value = mock_df
 
-        result = api.fetch_and_save_posts("http://fakeapi", "test_table")
+        result = api.fetch_and_save_posts("https://fakeapi", "test_table")
         self.assertTrue(result)
         mock_df.write.format.assert_called_with("delta")
 
     @patch("api.requests.get", side_effect=api.requests.exceptions.RequestException("API error"))
     @patch("api.SparkSession")
     def test_fetch_and_save_posts_failure(self, mock_spark, mock_get):
-        result = api.fetch_and_save_posts("http://fakeapi", "test_table")
+        result = api.fetch_and_save_posts("https://fakeapi", "test_table")
         self.assertFalse(result)
 
 
